000d4a4c920f113a84fb913408d9aa61ffbafce7
ryoherisson <ryhizw@gmail.com>
2020-12-20 02:08:04 +0900

    add select_gamma & unit test

--- prts/interfaces/time_series_metrics.py
+++ prts/interfaces/time_series_metrics.py
@@ -26,3 +26,34 @@ class InterfaceTimeSeriesMetrics:
         """
 
         return 1.0
+
+    def _gamma_select(self, gamma: str, overlap: int) -> float:
+        """The function of selecting the gamma value according to the parameters.
+
+        Args:
+            gamma: str
+                - 'one': the value 1
+                - 'reciprocal';: a reciprocal of the overlap
+                - 'udf_gamma': user defined gamma
+            overlap: int
+                overlap between real and pred
+
+        Returns:
+            float: the selected gamma value
+        """
+        assert type(overlap) == int, TypeError()
+
+        if gamma == "one":
+            return 1.0
+        elif gamma == "reciprocal":
+            if overlap > 1:
+                return 1.0 / overlap
+            else:
+                return 1.0
+        elif gamma == "udf_gamma":
+            if overlap > 1:
+                return 1.0 / self._udf_gamma()
+            else:
+                return 1.0
+        else:
+            ValueError(f"Expected one of one, reciprocal, udf_gamma. gamma type string: {gamma}")
