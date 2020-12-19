73acf19617c6f356fad6d7a8d92b769204a83460
Masanari Kimura <noconoco.lib@gmail.com>
2020-12-19 23:14:08 +0900

    add docstring & unit test

--- prts/time_series_metrics/precision.py
+++ prts/time_series_metrics/precision.py
@@ -9,8 +9,13 @@ class TimeSeriesPrecision(InterfaceTimeSeriesMetrics):
     """ This class calculates precision for time series """
 
     def __init__(self, beta=1.0, alpha=0.0, cardinality="one", bias="flat"):
-        """
-        docstring
+        """ Constructor
+
+        Args:
+            beta (float, optional): 0 < beta. Defaults to 1.0.
+            alpha (float, optional): 0 <= alpha <= 1. Defaults to 0.0.
+            cardinality (str, optional): ["one", "reciprocal", "udf_gamma"]. Defaults to "one".
+            bias (str, optional): ["flat", "front", "middle", "back"]. Defaults to "flat".
         """
 
         assert (alpha >= 0) & (alpha <= 1)
