bf3109c76393e63ce1352b86fac23a5caaef9aa8
Masanari Kimura <noconoco.lib@gmail.com>
2020-12-27 11:50:47 +0900

    add recall

--- prts/time_series_metrics/precision.py
+++ prts/time_series_metrics/precision.py
@@ -28,27 +28,6 @@ class TimeSeriesPrecision(InterfaceTimeSeriesMetrics):
         self.cardinality = cardinality
         self.bias = bias
 
-    def _update_precision(self, real_anomalies, predicted_anomalies):
-        precision = 0
-        if len(predicted_anomalies) == 0:
-            return 0
-        for i in range(len(predicted_anomalies)):
-            range_p = predicted_anomalies[i, :]
-            omega_reward = 0
-            overlap_count = [0]
-            for j in range(len(real_anomalies)):
-                range_r = real_anomalies[j, :]
-                omega_reward += self._compute_omega_reward(range_p, range_r, overlap_count, 0)
-            overlap_reward = self._gamma_function(overlap_count, 0) * omega_reward
-            if overlap_count[0] > 0:
-                existence_reward = 1
-            else:
-                existence_reward = 0
-
-            precision += self.alpha_p * existence_reward + (1 - self.alpha_p) * overlap_reward
-        precision /= len(predicted_anomalies)
-        return precision
-
     def score(self, real: np.ndarray, pred: np.ndarray) -> Any:
         """
 
@@ -67,3 +46,23 @@ class TimeSeriesPrecision(InterfaceTimeSeriesMetrics):
 
         return precision
 
+    def _update_precision(self, real_anomalies, predicted_anomalies):
+        precision = 0
+        if len(predicted_anomalies) == 0:
+            return 0
+        for i in range(len(predicted_anomalies)):
+            range_p = predicted_anomalies[i, :]
+            omega_reward = 0
+            overlap_count = [0]
+            for j in range(len(real_anomalies)):
+                range_r = real_anomalies[j, :]
+                omega_reward += self._compute_omega_reward(range_p, range_r, overlap_count, 0)
+            overlap_reward = self._gamma_function(overlap_count, 0) * omega_reward
+            if overlap_count[0] > 0:
+                existence_reward = 1
+            else:
+                existence_reward = 0
+
+            precision += self.alpha * existence_reward + (1 - self.alpha) * overlap_reward
+        precision /= len(predicted_anomalies)
+        return precision
