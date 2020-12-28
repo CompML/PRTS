31a2234f26c3980379760fe5b73e8fb57e2a093b
Masanari Kimura <noconoco.lib@gmail.com>
2020-12-29 00:08:17 +0900

    add test of recall function with ndarray

--- tests/test_recall.py
+++ tests/test_recall.py
@@ -1,8 +1,10 @@
+import unittest
+import numpy as np
+
 from prts.base.time_series_metrics import BaseTimeSeriesMetrics
 from prts.time_series_metrics.recall import TimeSeriesRecall
 from prts import ts_recall
 
-import unittest
 
 class TestRecall(unittest.TestCase):
     def test_recall_inherited_BaseTimeSeriesMetrics(self):
@@ -13,6 +15,15 @@ class TestRecall(unittest.TestCase):
             isinstance(recall, BaseTimeSeriesMetrics)
         )
 
+    def test_recall_function(self):
+        """Teest of ts_recall function.
+        """
+
+        real = np.array([1, 1, 0, 0, 0])
+        pred = np.array([1, 1, 1, 1, 0])
+
+        score = ts_recall(real, pred)
+        self.assertEqual(score, 1.0)
 
     def test_recall_function_with_list(self):
         """Teest of ts_recall function with list type arguments.
